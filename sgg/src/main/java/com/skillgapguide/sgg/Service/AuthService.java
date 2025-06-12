ac2f73a70bdc1003b09a1a3ebae9d20ee381efa3
hungngooo <ngongochung531@gmail.com>
2025-06-12 19:42:23 +0700

    Change gitignore and application.properties

--- sgg/src/main/java/com/skillgapguide/sgg/Service/AuthService.java
+++ sgg/src/main/java/com/skillgapguide/sgg/Service/AuthService.java
@@ -30,7 +30,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserStatusRepository userStatusRepository;
     private final EmailService emailService;
-    @Value("${application.base-url}") // Thêm vào application.properties: application.base-url=http://localhost:8080
+    @Value("${application.base-url}")
     private String baseUrl;
     @Transactional
     public String register(RegisterRequest request) {
