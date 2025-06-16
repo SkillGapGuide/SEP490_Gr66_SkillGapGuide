26a7bf5bf2e855519381cce8bc3a93213ba3c3fe
ttmbg2003 <trantuanminhbg2003@gmail.com>
2025-06-17 02:02:13 +0700

    fix feedback manage input change id into email

--- sgg/src/main/java/com/skillgapguide/sgg/Service/FeedbackService.java
+++ sgg/src/main/java/com/skillgapguide/sgg/Service/FeedbackService.java
@@ -2,6 +2,7 @@ package com.skillgapguide.sgg.Service;
 
 import com.skillgapguide.sgg.Dto.FeedbackDetailResponse;
 import com.skillgapguide.sgg.Dto.FeedbackListResponse;
+import com.skillgapguide.sgg.Entity.User;
 import com.skillgapguide.sgg.Repository.FeedbackRepository;
 import com.skillgapguide.sgg.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,18 @@ import java.util.List;
 public class FeedbackService {
     @Autowired
     private FeedbackRepository feedbackRepository;
+    @Autowired
+    private UserRepository userRepository;
     public List<FeedbackListResponse> getFeedbackList(int star){
         if(star == 0){
             return feedbackRepository.getAllFeedback();
         }
         return feedbackRepository.getAllFeedbackByStar(star);
     }
-    public FeedbackDetailResponse getFeedbackDetail(int userId){
-        return feedbackRepository.getFeedbackDetail(userId);
+    public FeedbackDetailResponse getFeedbackDetail(String email){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalStateException("Người dùng không tồn tại"));
+        return feedbackRepository.getFeedbackDetail(email);
     }
 
 }
