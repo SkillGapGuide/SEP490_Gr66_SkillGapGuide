f2c339e573ec0edc4f02d88a19ce07ecddc757a4
ttmbg2003 <trantuanminhbg2003@gmail.com>
2025-06-16 22:54:34 +0700

    add service for feedback

---
+++ sgg/src/main/java/com/skillgapguide/sgg/Controller/FeedbackController.java
@@ -0,0 +1,22 @@
+package com.skillgapguide.sgg.Controller;
+
+import com.skillgapguide.sgg.Response.EHttpStatus;
+import com.skillgapguide.sgg.Response.Response;
+import com.skillgapguide.sgg.Service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/feedback")
+public class FeedbackController {
+    @Autowired
+    private FeedbackService feedbackService;
+    @GetMapping("/getAll")
+    public Response<?> getAllFeedback(@RequestParam(defaultValue = "0") int star){
+        return new Response<>(EHttpStatus.OK,feedbackService.getFeedbackList(star));
+    }
+    @GetMapping("/getDetail/{id}")
+    public Response<?> getDetailFeedback(@PathVariable int id){
+        return new Response<>(EHttpStatus.OK,feedbackService.getFeedbackDetail(id));
+    }
+}
