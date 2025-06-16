11edea0684d752b04d925e05252585724f5e0a92
ttmbg2003 <trantuanminhbg2003@gmail.com>
2025-06-17 02:03:19 +0700

    add feature enable/disable account service

---
+++ sgg/src/main/java/com/skillgapguide/sgg/Dto/UserListResponse.java
@@ -0,0 +1,9 @@
+package com.skillgapguide.sgg.Dto;
+
+public interface UserListResponse {
+    String getName();
+    String getEmail();
+    String getPhone();
+    String getRole();
+    String getStatus();
+}
