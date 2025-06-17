79b2a71fa2a1b080307127525d6fa7bfe9a3d0b9
ttmbg2003 <trantuanminhbg2003@gmail.com>
2025-06-17 21:09:39 +0700

    add pageable for feedback service

--- sgg/src/main/java/com/skillgapguide/sgg/Repository/FeedbackRepository.java
+++ sgg/src/main/java/com/skillgapguide/sgg/Repository/FeedbackRepository.java
@@ -3,6 +3,8 @@ package com.skillgapguide.sgg.Repository;
 import com.skillgapguide.sgg.Dto.FeedbackDetailResponse;
 import com.skillgapguide.sgg.Dto.FeedbackListResponse;
 import com.skillgapguide.sgg.Entity.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,10 +12,10 @@ import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback,Integer> {
     @Query(value = "select u.email as email,f.content as content,f.star as star from Feedback as f join User as u on f.userId = u.userId")
-    List<FeedbackListResponse> getAllFeedback();
+    Page<FeedbackListResponse> getAllFeedback(Pageable pageable);
     @Query(value = "select u.email as email,f.content as content,f.star as star from Feedback as f join User as u on f.userId = u.userId" +
             " where f.star = :star")
-    List<FeedbackListResponse> getAllFeedbackByStar(int star);
+    Page<FeedbackListResponse> getAllFeedbackByStar(int star,Pageable pageable);
     @Query(value = "select u.email as email,f.content as content,f.star as star,u.fullName as name,f.createAt as time from Feedback as f join User as u on f.userId = u.userId" +
             " where u.email = :email")
     FeedbackDetailResponse getFeedbackDetail(String email);
--- sgg/src/main/java/com/skillgapguide/sgg/Service/FeedbackService.java
+++ sgg/src/main/java/com/skillgapguide/sgg/Service/FeedbackService.java
@@ -6,6 +6,9 @@ import com.skillgapguide.sgg.Entity.User;
 import com.skillgapguide.sgg.Repository.FeedbackRepository;
 import com.skillgapguide.sgg.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +19,12 @@ public class FeedbackService {
     private FeedbackRepository feedbackRepository;
     @Autowired
     private UserRepository userRepository;
-    public List<FeedbackListResponse> getFeedbackList(int star){
+    public Page<FeedbackListResponse> getFeedbackList(int star, Integer pageNo, Integer pageSize){
+        Pageable paging = PageRequest.of(pageNo, pageSize);
         if(star == 0){
-            return feedbackRepository.getAllFeedback();
+            return feedbackRepository.getAllFeedback(paging);
         }
-        return feedbackRepository.getAllFeedbackByStar(star);
+        return feedbackRepository.getAllFeedbackByStar(star,paging);
     }
     public FeedbackDetailResponse getFeedbackDetail(String email){
         User user = userRepository.findByEmail(email)
