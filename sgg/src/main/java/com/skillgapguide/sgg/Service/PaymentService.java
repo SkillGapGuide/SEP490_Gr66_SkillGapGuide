3a89850729054ab246d17b55e435d5924b1592b6
ttmbg2003 <trantuanminhbg2003@gmail.com>
2025-08-16 16:46:28 +0700

    fix set price for payment by qr

--- sgg/src/main/java/com/skillgapguide/sgg/Service/PaymentService.java
+++ sgg/src/main/java/com/skillgapguide/sgg/Service/PaymentService.java
@@ -127,6 +127,7 @@ public class PaymentService {
     }
     public void confirmPaymentCassio(String json) throws Exception {
         try {
+            System.out.println("Xác nhận thanh toán từ Cassio: " + json);
             String status = "SUCCESS";
             JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
             JsonArray jsonArray = (JsonArray) jsonObject.get("data");
